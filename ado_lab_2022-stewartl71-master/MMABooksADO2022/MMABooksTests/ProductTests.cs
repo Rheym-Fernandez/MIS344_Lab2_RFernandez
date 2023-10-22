ad805d3f639339e5d9aad543d8450e2fe5d116ae
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-10-22 16:55:04 -0700

    Begin ProductTest

--- ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductTests.cs
+++ ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductTests.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace MMABooksTests
 {
+    [TestFixture]
     public class ProductTests
     {
+        [Test]
     }
 }
