045a470bfd4eb38e2e0b0f277c6783c656be8077
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-10-22 08:42:21 -0700

    Wrote out TestConstructor().

--- ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/CustomerTests.cs
+++ ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/CustomerTests.cs
@@ -21,6 +21,27 @@ namespace MMABooksTests
             c = new Customer(1, "Mickey Mouse", "Disneyland Dr.", "Anaheim", "CA", "12345");
         }
 
+        [Test]
+
+        public void TestConstructor()
+        {
+            Assert.IsNotNull(def);
+            Assert.AreEqual(null, def.Name);
+            Assert.AreEqual(null, def.Address);
+            Assert.AreEqual(null, def.City);
+            Assert.AreEqual(null, def.State);
+            Assert.AreEqual(null, def.ZipCode);
+
+            Assert.IsNotNull(c);
+            Assert.AreNotEqual(null, c.Name);
+            Assert.AreNotEqual("Mickey Mouse", c.Name);
+            Assert.AreEqual(null, c.Address);
+            Assert.AreEqual(null, c.City);
+            Assert.AreEqual(null, c.State);
+            Assert.AreEqual(null, c.ZipCode);
+
+        }
+
 
     }
 }
