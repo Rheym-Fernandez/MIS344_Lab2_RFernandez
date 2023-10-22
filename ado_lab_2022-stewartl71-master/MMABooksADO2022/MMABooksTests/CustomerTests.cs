3282b4dfe284b7b7e134c8c5944fe30129c9bc0b
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-10-22 08:36:21 -0700

    Wrote Setup() function for CustomerTests.

--- ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/CustomerTests.cs
+++ ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/CustomerTests.cs
@@ -10,5 +10,17 @@ namespace MMABooksTests
     [TestFixture]
     public class CustomerTests
     {
+        private Customer def;
+        private Customer c;
+
+        [SetUp]
+        
+        public void SetUp()
+        {
+            def = new Customer();
+            c = new Customer(1, "Mickey Mouse", "Disneyland Dr.", "Anaheim", "CA", "12345");
+        }
+
+
     }
 }
