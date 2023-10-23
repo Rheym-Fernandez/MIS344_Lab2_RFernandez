b57d5b8fd9daf787a07dc5231e57bbf77d42ef9c
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-10-22 17:17:25 -0700

    Created the SetUp() fxn in product tests class.

--- ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductTests.cs
+++ ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using MMABooksBusinessClasses;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,54 @@ namespace MMABooksTests
     [TestFixture]
     public class ProductTests
     {
+        private Product def;
+        private Product c;
+
+        [SetUp]
+
+        public void SetUp()
+        {
+            def = new Product();
+            c = new Product(1, "Test Item", "50.00", "100");
+        }
+
         [Test]
+
+        public void TestConstructor()
+        {
+            Assert.IsNotNull(def);
+            Assert.AreEqual(null, def.Name);
+            Assert.AreEqual(null, def.Address);
+            Assert.AreEqual(null, def.City);
+            Assert.AreEqual(null, def.State);
+            Assert.AreEqual(null, def.ZipCode);
+
+            Assert.IsNotNull(c);
+            Assert.AreNotEqual(null, c.Name);
+            Assert.AreNotEqual(null, c.Name);
+            Assert.AreNotEqual(null, c.Address);
+            Assert.AreNotEqual(null, c.City);
+            Assert.AreNotEqual(null, c.State);
+            Assert.AreNotEqual(null, c.ZipCode);
+
+        }
+
+        [Test]
+
+        public void TestNameSetter()
+        {
+            c.Name = "Minnie, Mouse";
+            Assert.AreNotEqual("Mickey, Mouse", c.Name);
+            Assert.AreEqual("Minnie, Mouse", c.Name);
+        }
+
+        [Test]
+
+        public void TestNameLength()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => c.Name = "1234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789");
+        }
+
+
     }
 }
