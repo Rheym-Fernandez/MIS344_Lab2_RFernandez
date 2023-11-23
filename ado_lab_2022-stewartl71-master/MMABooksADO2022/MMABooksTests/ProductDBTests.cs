5e5da7d8a9c3ffcdf93b2f79b482ff6f143795cf
Git_Rheym_Fernandez <rheym.fernandez@oit.edu>
2023-11-22 23:09:24 -0800

    Wrote code for CRUD in ProductDBTest (Finished Writing Code but could not get some to work)

--- ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductDBTests.cs
+++ ado_lab_2022-stewartl71-master/MMABooksADO2022/MMABooksTests/ProductDBTests.cs
@@ -28,5 +28,25 @@ namespace MMABooksTests
             p = ProductDB.GetProduct(productCode);
             Assert.AreEqual("Test", p.Description);
         }
+
+        [Test]
+        public void UpdateTest()
+        {
+            //Need to identify code that allows me to update and save product changes
+            Product p = ProductDB.GetProduct("A4CS");
+            p.Description = "Test";
+            //ProductDB.UpdateProduct(p);
+            //ProductDB.SaveChanges();
+            p = ProductDB.GetProduct("A4CS");
+            Assert.AreEqual("Test", p.Description);
+
+        }
+        [Test]
+        public void TestDeleteProducts()
+        {   // Need to Save Delete Changes First?
+            Product p = ProductDB.GetProduct("A4VB");
+            ProductDB.DeleteProduct(p);
+            Assert.IsNotNull(ProductDB.GetProduct("A4VB"));
+        }
     }
 }
